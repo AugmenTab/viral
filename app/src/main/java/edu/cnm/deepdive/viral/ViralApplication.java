7c68d40abd05f2e47327d68f840c4bd1661d2c50
AugmenTab <tbaum92@live.com>
2020-11-08 15:58:45 -0700

    Added necessary logic to create database.

--- app/src/main/java/edu/cnm/deepdive/viral/ViralApplication.java
+++ app/src/main/java/edu/cnm/deepdive/viral/ViralApplication.java
@@ -2,6 +2,8 @@ package edu.cnm.deepdive.viral;
 
 import android.app.Application;
 import com.facebook.stetho.Stetho;
+import edu.cnm.deepdive.viral.model.service.ViralDatabase;
+import io.reactivex.schedulers.Schedulers;
 
 public class ViralApplication extends Application {
 
@@ -9,6 +11,8 @@ public class ViralApplication extends Application {
   public void onCreate() {
     super.onCreate();
     Stetho.initializeWithDefaults(this);
+    ViralDatabase.setContext(this);
+    ViralDatabase.getInstance().getFriendDao().delete().subscribeOn(Schedulers.io()).subscribe();
   }
 
 }
