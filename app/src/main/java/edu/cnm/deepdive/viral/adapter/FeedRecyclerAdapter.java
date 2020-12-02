871deb64f7456bf5941c61e394cdc5cd83445d67
AugmenTab <tbaum92@live.com>
2020-12-02 11:54:02 -0700

    Began work on RecyclerViews.

---
+++ app/src/main/java/edu/cnm/deepdive/viral/adapter/FeedRecyclerAdapter.java
@@ -0,0 +1,94 @@
+package edu.cnm.deepdive.viral.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Picasso.Listener;
+import edu.cnm.deepdive.viral.R;
+import edu.cnm.deepdive.viral.databinding.ItemPostBinding;
+import edu.cnm.deepdive.viral.model.entity.Action;
+import edu.cnm.deepdive.viral.model.entity.ActionResponse;
+import edu.cnm.deepdive.viral.model.entity.ActionTaken;
+import edu.cnm.deepdive.viral.model.entity.Friend;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeedRecyclerAdapter extends RecyclerView.Adapter {
+
+  private final Context context;
+  private final List<ActionTaken> posts;
+  private final DateFormat formatter;
+  private final LayoutInflater inflater;
+  private final Action postAction;
+  private final ActionResponse postComment;
+  private final Friend poster;
+
+  public FeedRecyclerAdapter(@NonNull Context context, @NonNull Action postAction,
+      @NonNull ActionResponse postComment, @NonNull Friend poster) {
+    this.context = context;
+    this.postAction = postAction;
+    this.postComment = postComment;
+    this.poster = poster;
+    posts = new ArrayList<>();
+    formatter = DateFormat.getInstance();
+    inflater = LayoutInflater.from(context);
+  }
+
+  public List<ActionTaken> getPosts() {
+    return posts;
+  }
+
+  @NonNull
+  @Override
+  public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    ItemPostBinding binding = ItemPostBinding.inflate(inflater, parent, false);
+    return new Holder(binding);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull Holder holder, int position) {
+    holder.bind(position);
+  }
+
+  @Override
+  public int getItemCount() {
+    return posts.size();
+  }
+
+  class Holder extends RecyclerView.ViewHolder {
+
+    private final ItemPostBinding binding;
+
+    private Holder(@NonNull ItemPostBinding binding) {
+      super(binding.getRoot());
+      this.binding = binding;
+    }
+
+    private void bind(int position) {
+      ActionTaken post = posts.get(position);
+      Picasso picasso = new Picasso.Builder(context).listener(new Listener() {
+        @Override
+        public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
+          Toast.makeText(context, R.string.image_load_failure_message, Toast.LENGTH_SHORT);
+        }
+      }).build();
+      picasso.load(poster.getProfilePicture()).into(binding.postProfilePicture);
+      binding.postPoster.setText(poster.getName());
+      binding.postContents.setText(postAction.getContent());
+      String timestamp = String.format(
+          "Posted %s @ %s", formatter.format(post.getTimestamp()), poster.getAddress());
+      binding.postTimestamp.setText(timestamp);
+//      binding.buttonFocus.setOnClickListener(); REDIRECTS TO FEED FOR THAT FRIEND
+//      binding.buttonMessage.setOnClickListener(); CREATES MESSAGE, THEN REDIRECTS TO MESSAGE PAGE
+      // TODO Add comments.
+    }
+
+  }
+
+}
---
+++ app/src/main/java/edu/cnm/deepdive/viral/adapter/MessagesRecyclerAdapter.java
@@ -0,0 +1,68 @@
+package edu.cnm.deepdive.viral.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import edu.cnm.deepdive.viral.databinding.ItemMessageFriendBinding;
+import edu.cnm.deepdive.viral.model.entity.Action;
+import edu.cnm.deepdive.viral.model.entity.ActionTaken;
+import edu.cnm.deepdive.viral.model.entity.Friend;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessagesRecyclerAdapter extends RecyclerView.Adapter {
+
+  private final Context context;
+  private final List<ActionTaken> messages;
+  private final DateFormat formatter;
+  private final LayoutInflater inflater;
+  private final Action messageAction;
+  private final Friend friend;
+
+  public MessagesRecyclerAdapter(
+      @NonNull Context context, @NonNull Action messageAction, @NonNull Friend friend) {
+    this.context = context;
+    this.messageAction = messageAction;
+    this.friend = friend;
+    messages = new ArrayList<>();
+    formatter = DateFormat.getInstance();
+    inflater = LayoutInflater.from(context);
+  }
+
+  @NonNull
+  @Override
+  public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    ItemMessageFriendBinding binding = ItemMessageFriendBinding.inflate(inflater, parent, false);
+    return new Holder(binding);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull Holder holder, int position) {
+    holder.bind(position);
+  }
+
+  @Override
+  public int getItemCount() {
+    return messages.size();
+  }
+
+  class Holder extends RecyclerView.ViewHolder {
+
+    private final ItemMessageFriendBinding binding;
+
+    private Holder(@NonNull ItemMessageFriendBinding binding) {
+      super(binding.getRoot());
+      this.binding = binding;
+    }
+
+    private void bind(int position) {
+      ActionTaken message = messages.get(position);
+      binding.messageFriend.setText(messageAction.getContent());
+    }
+
+  }
+
+}
