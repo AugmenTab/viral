a0f8f0cff4ab4a2a15c8041ec2f0f5ce26d605ee
AugmenTab <tbaum92@live.com>
2020-12-03 14:16:25 -0700

    Made attempt at shared preferences.

--- app/src/main/java/edu/cnm/deepdive/viral/controller/NewGameActivity.java
+++ app/src/main/java/edu/cnm/deepdive/viral/controller/NewGameActivity.java
@@ -3,23 +3,22 @@ package edu.cnm.deepdive.viral.controller;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import androidx.lifecycle.LiveData;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.android.material.textfield.TextInputLayout.OnEditTextAttachedListener;
 import edu.cnm.deepdive.viral.R;
 import edu.cnm.deepdive.viral.databinding.ActivityNewGameBinding;
 import edu.cnm.deepdive.viral.model.entity.Game;
-import edu.cnm.deepdive.viral.service.ViralDatabase;
 import edu.cnm.deepdive.viral.viewmodel.NewGameViewModel;
 
+/**
+ * The NewGameActivity acts as a landing page for new users, and for users without an ongoing game.
+ * It is designed to look like a login screen, where the player can enter a username and "create an
+ * account," which starts a new {@link Game}.
+ */
 public class NewGameActivity extends AppCompatActivity {
 
   private ActivityNewGameBinding binding;
@@ -27,18 +26,16 @@ public class NewGameActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-//    LiveData<Game> currentGame = ViralDatabase.getInstance().getGameDao().selectCurrentGame();
-//    if (currentGame.getValue().getEndTime() != null) {
-//      switchActivity();
-//    }
+    // TODO Check if a game with a null end time already exists. If one does, redirect to the
+    //  MainActivity.
     binding = ActivityNewGameBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
     NewGameViewModel viewModel = new ViewModelProvider(this).get(NewGameViewModel.class);
     viewModel.getThrowable().observe(this, (throwable) ->
         Log.e(getClass().getSimpleName(), throwable.getMessage(), throwable));
     SharedPreferences preferences = getSharedPreferences("UserPreferences", Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = preferences.edit();
     String preferredUsername = preferences.getString("username", "");
-    Toast.makeText(NewGameActivity.this, "Username: " + preferredUsername, Toast.LENGTH_LONG);
     binding.accountNameInput.setText(preferredUsername);
     binding.newGameButton.setOnClickListener((v) -> {
       String username = binding.accountNameInput.getText().toString().trim();
@@ -46,7 +43,7 @@ public class NewGameActivity extends AppCompatActivity {
         Toast.makeText(
             NewGameActivity.this, R.string.no_username_message, Toast.LENGTH_SHORT).show();
       } else {
-        preferences.edit().putString("username", username);
+        editor.putString("username", username);
         viewModel.newGame(username, 0);
         switchActivity();
       }
