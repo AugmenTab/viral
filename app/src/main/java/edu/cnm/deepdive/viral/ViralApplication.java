6377c0649d2efd852dca3ee28bf7a0b22a0817d9
AugmenTab <tbaum92@live.com>
2020-12-03 16:17:36 -0700

    Cleaned & added Javadocs for application class.

--- app/src/main/java/edu/cnm/deepdive/viral/ViralApplication.java
+++ app/src/main/java/edu/cnm/deepdive/viral/ViralApplication.java
@@ -1,14 +1,18 @@
 package edu.cnm.deepdive.viral;
 
 import android.app.Application;
-import androidx.lifecycle.LiveData;
 import com.facebook.stetho.Stetho;
-import edu.cnm.deepdive.viral.model.entity.Game;
 import edu.cnm.deepdive.viral.service.ViralDatabase;
 import io.reactivex.schedulers.Schedulers;
 
+/**
+ * The main application class for the Viral application.
+ */
 public class ViralApplication extends Application {
 
+  /**
+   * The constructor initializes the database with an empty call.
+   */
   @Override
   public void onCreate() {
     super.onCreate();
